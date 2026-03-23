e65afc8ef1469439e5031bbb418ba9a127257679
RomaKorol-Js <214110988+RomaKorol-Js@users.noreply.github.com.>
2026-03-23 14:35:34 +0200

    init commit

--- Lab_7/Part 1-3/big.js
+++ Lab_7/Part 1-3/big.js
@@ -5,9 +5,7 @@ async function fetchData() {
     "https://api.census.gov/data/2020/acs/acs5/profile?get=NAME&for=county:*",
   );
   let json = await response.json();
-  counties = json
-    .slice(1)
-    .map((res) => ({ name: res[0], code: res[1] + res[2] }));
+  counties = json.slice(1).map((res) => ({ name: res[0], code: res[1] + res[2] }));
 }
 
 function getCode(name) {
