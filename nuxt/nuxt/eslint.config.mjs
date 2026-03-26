630e7401ea03a89f9be362e5d0982e9b9a0af4d2
RomaKorol-Js <214110988+RomaKorol-Js@users.noreply.github.com.>
2026-03-26 19:25:33 +0200

    feat: add table with products

--- nuxt/nuxt/eslint.config.mjs
+++ nuxt/nuxt/eslint.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt(
+  eslintConfigPrettier
   // Your custom configs here
-)
+);
---
+++ nuxt/nuxt/server/Api/items.js
@@ -0,0 +1,80 @@
+export default defineEventHandler(() => {
+  return [
+    {
+      title: 'Starter - Annual',
+      period: '3-days free then:',
+      price_for_month: '$83.25',
+      payment_frequency: 'month',
+      price_for_year: '$1,188',
+      discound_price: '$999',
+      savings: '$189 in savings',
+      text_for_button: 'Try It Free',
+      additionals: [
+        [['Primary user only'], ['(extra team members for $35/month)']],
+        ['Save unlimited properties'],
+        [['10,000 exports'], ['(additional exports at $0.02 each)']],
+        [['500 free skip traces'], ['(additional skip tracing at $0.08 each)']],
+        ['Imports $0.01'],
+        ['FREE daily product trainings and support'],
+        ['Full suite of next-gen investing tools'],
+        ['Industry first AI powered comp tool'],
+        ['Includes dedicated support agent']
+      ]
+    },
+    {
+      title: 'Team - Annual',
+      period: '3-days free then:',
+      price_for_month: '$207.50',
+      payment_frequency: 'month',
+      price_for_year: '$2,988',
+      discound_price: '$2,490',
+      savings: '$498 in savings',
+      text_for_button: 'Try It Free',
+      additionals: [
+        [
+          ['Primary user + 2 free team members'][
+            '(extra team members for $25/month)'
+          ]
+        ],
+        ['Save unlimited properties'],
+        ['50,000 exports]', ['(additional exports at $0.01 each)']],
+        [
+          ['1,000 free skip traces'],
+          ['(additional skip tracing at $0.08 each)']
+        ],
+        ['Imports $0.01'],
+        ['FREE daily product trainings and support'],
+        ['Full suite of next-gen investing tools'],
+        ['Industry first AI powered comp tool'],
+        ['Includes dedicated support agent']
+      ]
+    },
+    {
+      title: 'Business - Annual',
+      period: '3-days free then:',
+      price_for_month: '$457.50',
+      payment_frequency: 'month',
+      price_for_year: '$6,588',
+      discound_price: '$5,490',
+      savings: '$1,098 in savings',
+      text_for_button: 'Try It Free',
+      additionals: [
+        [
+          ['Primary user + 6 free team members'],
+          ['(extra team members for $20/month)']
+        ],
+        ['Save unlimited properties'],
+        [['100,000 exports'], ['(additional exports at $0.01 each)']],
+        [
+          ['2,000 free skip traces'],
+          ['(additional skip tracing at $0.08 each)']
+        ],
+        ['Imports $0.01'],
+        ['FREE daily product trainings and support'],
+        ['Full suite of next-gen investing tools'],
+        ['Industry first AI powered comp tool'],
+        ['Includes dedicated support agent']
+      ]
+    }
+  ];
+});
